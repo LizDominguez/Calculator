b320f616c6e7e732267e9ee84c3796dc04af5499
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-07-08 19:44:37 -0400

    ON and AC functionality

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
+var num = 0;
+var display = document.querySelector(".screen");
+var enter = document.querySelector(".enter");
+var buttons = document.querySelectorAll(".buttons");
+
+init();
+
+function init(){
+  
+  display.textContent = " ";
+  
+  for(var i = 0; i < buttons.length; i++){
+    
+    buttons[i].addEventListener("click", function(){
+    if (this.textContent === "ON"){
+      ac();
+    }
+  
+  
+});
+    
+  }
+  
+  
+}
+
+function ac(){
+  num = 0;
+  display.textContent = num;
+}
+
+enter.addEventListener("click", function(){
+  
+  
+});
+
+function add(num) {
+  var number = 0;
+  number += num;
+}
+
+function subtract(num) {
+  var number = 0;
+  number -= num;
+}
