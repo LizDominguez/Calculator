6d39b1deae7d23ee8851fcb1c94d6d9f9ca92181
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-07-08 20:28:34 -0400

    displays numbers

--- main.js
+++ main.js
@@ -1,48 +1,123 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
-var num = 0;
+var num;
 var display = document.querySelector(".screen");
 var enter = document.querySelector(".enter");
 var buttons = document.querySelectorAll(".buttons");
+var resultStr = "";
 
 init();
 
 function init(){
   
-  display.textContent = " ";
+  off(); //calculator is off
+  checkButtons(); //probe buttons for input
   
-  for(var i = 0; i < buttons.length; i++){
+}
+
+
+function checkButtons() {
+  
+   for(var i = 0; i < buttons.length; i++){
     
     buttons[i].addEventListener("click", function(){
+     
+    //Turn ON
     if (this.textContent === "ON"){
       ac();
     }
+    
+    //All clear
+    if (this.textContent === "AC"){
+      ac();
+    }
+      
+    //0
+    if (this.textContent === "0"){
+      ac();
+    }
+      
+    //1
+    if (this.textContent === "1"){
+      num = 1;
+      displayNum();
+    }
+      
+    //2
+    if (this.textContent === "2"){
+      num = 2;
+      displayNum();
+    }
+      
+    //3
+    if (this.textContent === "3"){
+      num = 3;
+      displayNum();
+    }
+      
+    //4
+    if (this.textContent === "4"){
+      num = 4;
+      displayNum();
+    }
+      
+    //5
+    if (this.textContent === "5"){
+      num = 5;
+      displayNum();
+    }
+      
+    //6
+    if (this.textContent === "6"){
+      num = 6;
+      displayNum();
+    }
+      
+    //7
+    if (this.textContent === "7"){
+      num = 7;
+      displayNum();
+    }
+      
+    //8
+    if (this.textContent === "8"){
+      num = 8;
+      displayNum();
+    }
+      
+    //9
+    if (this.textContent === "9"){
+      num = 9;
+      displayNum();
+    }
+
   
   
 });
     
   }
   
-  
 }
+
+function off(){
+  display.textContent = " ";
+}
+
 
 function ac(){
   num = 0;
   display.textContent = num;
 }
 
-enter.addEventListener("click", function(){
-  
-  
-});
-
-function add(num) {
-  var number = 0;
-  number += num;
+function displayNum(){
+  display.textContent = num;
 }
 
-function subtract(num) {
-  var number = 0;
-  number -= num;
+
+function numbers(num){
+  var numbers = [];
+  numbers.push(num);
+  
+  return numbers;
 }
